e4f016ad4636220dca886c5cdcfadd9f7682fbf8
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2017-12-16 12:44:53 -0800

    deleted cloned loops for letter checker then used join and split during inner html to remove commas and add spaces

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,28 @@
-// Known Bugs: now for some reason, a random letter in the word becomes impossible to use, classes are not being added though I am using jQuery-sick of javascript-feeling like I try a million or so  work arounds rather than straight forward jQuery ಥ_ಥ
-// 1) startGame() function - how we will start and restart the game
-// $(document).ready(function() {
-// Variable declaration
+
+//Variable Declaration
 var wordbank = ["marduk", "gilgamesh", "enkiddu", "tiamat"];
-var hiddenWord = [];
+var hiddenWordArr = [];
 var lives = 9;
 var usedLetters = [];
 var randomWord;
-var letters;
+var lettersArr;
 var letterCounter = 0;
 var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
 function startGame(){
   // Chooses random word from wordbank array
   var word = [Math.floor(Math.random()*wordbank.length)];
   randomWord = wordbank[word];
 
   // Takes string and splits every character into different index in array
-  letters = randomWord.split("");
+  lettersArr = randomWord.split("");
 
   // Pushes underscore in the Array
-  for (var i = 0; i<letters.length; i++){
-    // hiddenWord.split("");
-    hiddenWord.push("_");
+  for (var i = 0; i<lettersArr.length; i++){
+    // hiddenWordArr.split("");
+    hiddenWordArr.push("_");
   }
-  document.getElementById("chosenWord").innerHTML = hiddenWord;
+  document.getElementById("chosenWord").innerHTML = hiddenWordArr.join(" ");
 }
 
 // 2) checkedLetters() function - it's where we will do all the comparison for matches (edited)
@@ -31,73 +30,43 @@ function checkedLetters(){
   document.onkeyup = function (event){
     var makeWord;
     var guess = event.key;
-    for(var l = 0; l <= usedLetters.length-1; l++)
-    {
-      //Winnie I couldn't figure out how to implement a check for the keycodes-I also tried a separate if check populating an array with keyCodes
-      if(usedLetters[l].indexOf(guess) != -1){
-      // || (guess.keyCode < 65 && guess.keyCode > 91)){
-        return false;
-      }
-    } // FALSE INPUT FOR
+    if (event.keyCode < 65 ||  event.keyCode >= 91){
+      return false;
+    }
+    console.log(event);
     var guessLowercase = guess.toLowerCase();
+    if (usedLetters.includes(guessLowercase) ){
+      // display message to user that they've guessed this letter
+      return false;
+    }
     usedLetters.push(guessLowercase);
-
     document.getElementById("typedLetters").innerHTML = usedLetters;
     document.getElementById("chances").innerHTML = lives;
-    letterCounter=0;
-    //Loop for hiddenWord
-    for (var j = 0; j<letters.length; j++){
 
-      if (guessLowercase === letters[j]){
-        hiddenWord[j]=letters[j];
-        document.getElementById("chosenWord").innerHTML = hiddenWord;
-        makeWord = hiddenWord.join("");
-        var lastWord = hiddenWord.join("");
 
+      //Loop for hiddenWordArr
+    for (var j = 0; j < lettersArr.length; j++){
+
+      if (guessLowercase === lettersArr[j]){
+        hiddenWordArr[j] = lettersArr[j];
       }
     }
-    for (var k = 0; k < randomWord.length; k++) {
-      if (guessLowercase == hiddenWord[k]){
-        letterCounter+=1;
-      }
-    }
+    console.log('this is hiddenWordArr:' + hiddenWordArr);
+    document.getElementById("chosenWord").innerHTML = hiddenWordArr.join(" ");
+
     if (letterCounter === 0){
       lives -= 1;
       document.getElementById("chances").innerHTML=lives;
     }
-    //ಥ﹏ಥ
-    if(makeWord === chosenWord){
+
+    if(randomWord === hiddenWordArr.join("")){
       alert("You win!");
       var refreshButton = $("<button>");
       refreshButton.addClass("display");
       $("gameOver").append(refreshButton);
       //banner below/above saying congratulating. Clone for lose
       document.getElementById("win").innerHTML="You Win, yesss!";
-
     }
-    if (lives<1){
-      var target = document.getElementById("gameOver");
-      var button = document.getElementById("button");
-
-      // 3) roundComplete() function - Here we will have all of the code that needs to be run after each guess is made.
-      // doesn't work yet-look up ಠಿ_ಠ
-      function makeVisible() {
-        var classContent = target.className;
-        gameOver.className = classContent.replace("invisible", "").trim();
-      }
-      button.addEventListener("click", makeVisible, true);
-    }
-    //ಥ﹏ಥ
-    if(makeWord === hiddenWord){
-      alert("You win!");
-      var refreshButton = $("<button>");
-      refreshButton.addClass("display");
-      $("gameOver").append(refreshButton);
-      //banner below/above saying congratulating. Clone for lose
-      document.getElementById("win").innerHTML="You Win, yesss!";
-
-    }
-
   }
 }
 
