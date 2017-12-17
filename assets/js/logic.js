f5157947878458439e2ce5d6aafe10e1099b4425
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2017-12-17 09:46:51 -0800

    output is hidden for start button. styling next

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -15,6 +15,12 @@ var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 //start game function finds a word
 
 function startGame(){
+  document.getElementById('openKeyboard').addEventListener('click', function(){
+    var inputElement = document.getElementById('hiddenInput');
+    inputElement.style.visibility = 'visible'; // unhide the input
+    inputElement.focus(); // focus on it so keyboard pops
+    inputElement.style.visibility = 'hidden'; // hide it again
+});
   // Chooses random word from wordbank array
   var word = [Math.floor(Math.random()*wordbank.length)];
   randomWord = wordbank[word];
